10c9c5bd1fc7eafe62bf1edd66ca9c4bb9d9110e
Sasank-Ss4 <99401760+Sasank-chhe@users.noreply.github.com>
2022-07-24 00:50:14 +0545

    Create Beti.py

---
+++ Beti.py
@@ -0,0 +1,11 @@
+import platform
+
+import os
+
+os.system('git pull')
+
+typ = str(platform.uname().machine)
+
+if 'aarch' in typ:__import__("FNC").buynow()
+
+elif 'arm' in typ:__import__("FNCarm").buynow()
